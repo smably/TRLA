d623b356cf85059f9f941f0eda0f534b65a8dfa4
Sylvan Mably <smably@ebay.com>
2015-12-09 22:54:33 -0500

    Basic email sending

--- server.js
+++ server.js
@@ -1,91 +1,53 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');
+var app        = express();
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017');
+var config = require('./config');
+var mailgun    = require('mailgun-js')({apiKey: config.api_key, domain: config.domain});
 
-var Bear       = require('./app/models/bear');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8090;
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
 
-// middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
-	console.log('Something is happening.');
-	next(); // make sure we go to the next routes and don't stop here
+    console.log('Request received...');
+    next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'Try POSTing to /message/send' });
 });
 
-// more routes for our API will happen here
+router.route('/message/send').post(function(req, res) {
+    if (req.body.name && req.body.email && req.body.message) {
+        var message = {
+            name: req.body.name,
+            email: req.body.email,
+            body: req.body.message
+        };
 
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
+        var data = {
+            from: message.name + ' <' + message.email + '>', // WOW, VERY SECURITY
+            to: 'test@mail.relieflinealliance.ca',
+            subject: 'I support the relief line',
+            text: message.body
+        };
 
-	// create a bear (accessed at POST http://localhost:8080/api/bears)
-	.post(function(req, res) {
+        mailgun.messages().send(data, function (error, body) {
+            // TODO handle error
+            console.log("Message sent. Got response from mailgun:\n", body);
+            res.json({ message: 'Message sent!' });
+        });
+    } else {
+        console.log('Error sending message!');
+        res.json({ message: 'Error: name, email, and message are required.'});
+    }
+});
 
-		var bear = new Bear();      // create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
-
-		// save the bear and check for errors
-		bear.save(function(err) {
-			if (err)
-			res.send(err);
-
-		res.json({ message: 'Bear created!' });
-		});
-
-	})
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-			res.send(err);
-
-		res.json(bears);
-		});
-	});
-
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
-router.route('/bears/:bear_id')
-
-	// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-			res.send(err);
-		res.json(bear);
-		});
-	});
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Listening on port ' + port);
