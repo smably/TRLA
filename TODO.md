1bf212e2f2310640b4e5ac971f53591761e7e57f
Sylvan Mably <smably@ebay.com>
2015-12-17 18:34:22 -0500

    Slack integration + other improvements

---
+++ TODO.md
@@ -0,0 +1,14 @@
+### Email form TODO
+
+- [x] Verify submission with Recaptcha
+- [x] Send contents of form to council mailing list via Mailgun API
+- [x] Add sender info to supporters mailing list via API
+- [x] Add contents of message to Slack channel via Webhook API
+- [ ] Show newsletter/volunteer sign-up status in Slack
+- [ ] Change mailing list name from "newsletter" to "supporters"
+- [ ] Check for existence of email and refuse to send if already sent
+- [ ] Add customized header and footer with variable substitution
+- [ ] Better form validation (test in Safari/Mobile Safari)
+- [ ] Preview button to show email (in modal?) before sending
+- [ ] Get it hosted!
+
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var bodyParser = require('body-parser');
 var recaptcha  = require('express-recaptcha');
 var validator  = require('validator');
 var template   = require('es6-template-strings');
-var mailgun    = require('mailgun-js')({ apiKey: config.api_key, domain: config.domain });
+var Slack      = require('node-slack');
+var mailgun    = require('mailgun-js')({ apiKey: config.mailgun.api_key, domain: config.mailgun.domain });
 
 app.use(express.static('static'));
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 recaptcha.init(config.recaptcha.site_key, config.recaptcha.secret_key);
+var slack = new Slack(config.slack.hook_url);
 
 var port = process.env.PORT || 8090;
 
@@ -27,8 +29,13 @@ app.post('/message/send', recaptcha.middleware.verify, function(req, res) {
 });
 
 function sendEmail(req, res) {
-    if (!req.body.name) {
-        error(res, 'Error: name is required.');
+    if (!req.body.firstname) {
+        error(res, 'Error: first name is required.');
+        return;
+    }
+
+    if (!req.body.lastname) {
+        error(res, 'Error: last name is required.');
         return;
     }
 
@@ -37,26 +44,29 @@ function sendEmail(req, res) {
         return;
     }
 
+    if (!req.body.postalcode) {
+        error(res, 'Error: postal code is required.');
+        return;
+    }
+
     if (!req.body.message) {
         error(res, 'Error: message is required.');
         return;
     }
 
-    if (!validator.isEmail(res.body.email)) {
+    if (!validator.isEmail(req.body.email)) {
         error(res, 'Error: email is invalid.');
     }
 
-    var message = {
-        name: req.body.name,
-        email: req.body.email,
-        body: req.body.message
-    };
+    if (!/^[a-zA-Z]\d[a-zA-Z]\s*\d[a-zA-Z]\d$/.test(req.body.postalcode)) {
+        error(res, 'Error: postal code is invalid.');
+    }
 
     var data = {
-        from: template('${name} <${email}>', { name: message.name, email: message.email }),
+        from: template('${firstName} ${lastName} <${email}>', { firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email }),
         to: 'test@mail.relieflinealliance.ca',
         subject: 'I support the relief line',
-        text: message.body
+        text: req.body.message
     };
 
     mailgun.messages().send(data, function (error, body) {
@@ -64,6 +74,25 @@ function sendEmail(req, res) {
             res.json({ message: 'Error: could not send message.' });
         } else {
             console.log("Message sent. Got response from mailgun:\n", body);
+
+            var user = {
+                subscribed: !!req.body.newsletter,
+                address: req.body.email,
+                name: req.body.firstname + " " + req.body.lastname,
+                vars: {
+                    postalCode: req.body.postalcode,
+                    lawnsign: !!req.body.lawnsign
+                }
+            };
+
+            mailgun.lists('newsletter@mail.relieflinealliance.ca').members().create(user, function (err, data) {
+                if (err) {
+                    console.log(err, data);
+                }
+            });
+
+            slack.send({ text: template('*From:* ${from}\n*Message:* ${text}', data) });
+
             res.json({ message: 'Message sent!' });
         }
     });
